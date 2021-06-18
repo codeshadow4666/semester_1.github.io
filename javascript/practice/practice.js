04a9ed256b193b45179aa1ad875910aa1b59f269
Shadow <82682330+codeshadow4666@users.noreply.github.com>
2021-06-18 14:59:41 +0500

    ok ha

--- javascript/practice/practice.js
+++ javascript/practice/practice.js
@@ -1,20 +1,5 @@
-const date=new Date();
-let start,end;
-start=date.getTime();
-
-
-console.log(start);
-let totaltime=()=>{
- let total=   (end-start/1000); 
-console.log(total);
-}
-setTimeout(()=>{
-    console.log("ok here");
-    enddate();
-console.log(totaltime());
-
-},5000)
-function enddate(){
-    const date=new Date();
-    end=date.getTime();
-}
+let a=120;
+setInterval(() => {
+    console.log(a/60);
+    a--;
+}, 1000);
---
+++ javascript/practice/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+para2=para2.trim();
+// para2=para2.replace(/\s{2,}/g,' ');;
